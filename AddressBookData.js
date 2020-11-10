6e6d66845b0bf15d19da1eae1f11e4f161928157
kkumar2_be16@thapar.edu <kkumar2_be16@thapar.edu>
2020-11-10 23:25:02 +0530

    [Kirti]Added:ability to validate the fields of the class

--- AddressBookData.js
+++ AddressBookData.js
@@ -16,54 +16,84 @@ class AddressBookData{
     //getter and setter method
     get f_name() {return this._fName;}
     set f_name(fName){
-        
-        this._fName=fName;
+        const regx=RegExp("^[A-Z]{1}[a-z]{2,}$");
+        if(regx.test(fName))
+          this._fName=fName;
+        else
+        throw "Invalid First Name";
     }
     get l_name() {return this._lName;}
     set l_name(lName){
-        
-        this._lName=lName;
+        const regx=RegExp("^[A-Z]{1}[a-z]{2,}$");
+        if(regx.test(lName))
+          this._lName=lName;
+        else
+        throw "Invalid Last Name";
     }
     get address(){
         return this._address;
     }
     set address(address){
-        this._address = address;
+        const regx=RegExp("^[A-Z]{1,}[a-z]{3,}")
+        if(regx.test(address))
+          this._address = address;
+        else
+        throw "Invalid Address";
     }
 
     get city(){
         return this._city;
     }
     set city(city){
-        this._city = city;
+        const regx=RegExp("^[A-Z]{1,}[a-z]{3,}")
+        if(regx.test(city))
+          this._city = city;
+        else
+        throw "Invalid City";
     }
 
     get state(){
         return this._state;
     }
     set state(state){
-        this._state = state;
+        const regx=RegExp("^[A-Z]{1,}[a-z]{3,}")
+        if(regx.test(state))
+          this._state = state;
+        else
+        throw "Invalid State";
     }
 
     get zip(){
         return this._zip;
     }
     set zip(zip){
-        this._zip = zip;
+        const regexZip = RegExp("^[0-9]{3}\\s{0,1}[0-9]{3}$");
+        if(regexZip.test(zip))
+            this._zip = zip;
+        else
+            throw "Invalid ZipCode"; 
     }
 
     get ph_no(){
         return this._phNo;
     }
     set ph_no(phNo){
-        this._phNo = phNo
+        const regx=RegExp("^[0-9]{2}[ ]{1}[1-9]{1}[0-9]{9}");
+        if(regx.test(phNo))
+         this._phNo = phNo;
+        else 
+         throw "Invalid number ";
     }
 
     get email(){
         return this._email
     }
     set email(email){
-        this._email = email
+        const regexEmail = RegExp("^([a-z]){1,}[a-z0-9]*([.+_-]){0,1}[0-9a-z]+(@){1}([0-9a-z]+)(\\.([a-z]){2,}){1}(\\.[a-z]{2,})?$")
+        if(regexEmail.test(email))
+            this._email = email;
+        else
+            throw "Invalid Email"; 
     }
 
     //method
@@ -71,5 +101,9 @@ class AddressBookData{
         return "FirstName = " + this.fName + ", LastName = " + this.lName + ", Address = " + this.address + ", City = " + this.city + ", State = " + this.state + ", Zipcode = " + this.zip + ", PhoneNo = " + this.phNo + ", Email = " + this.email
     }
 }
-let addressBookData = new AddressBookData("abc", "def", "1234 street", "City1", "State1", "121331", "9999999999", "abc@gmail.com")
-console.log(addressBookData.toString());
+try{
+ let addressBookData = new AddressBookData("Kirti", "957umar", "House no", "City1", "State1", "332315", "99 9999999999", "abc@gmail.com")
+ console.log(addressBookData.toString());
+}catch(e)
+ {  console.log(e);
+    }
